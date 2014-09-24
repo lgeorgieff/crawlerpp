dce387293e4b6793017ea7d351db319d9a077168
Lukas Georgieff <lukas.georgieff@hotmail.com>
2014-09-24 20:35:10 +0200

    finalized build-integration of net-cpplib

--- src/tests.cpp
+++ src/tests.cpp
@@ -1,8 +1,21 @@
-#include "headers/uri.h"
+#include "uri.h"
 
+#include <network/uri.hpp>
+
+#include <iostream>
+
+using std::cout;
+using std::endl;
 
 int main(){
-
+  cout << endl;
+  cout << "entering tests.main" << endl;
   
+  network::uri instance("http://cpp-netlib.github.com/");
+  cout << "scheme: " << instance.scheme().get() << endl
+       << "host: " << instance.host().get() << endl
+       << "path: " << instance.path().get() << endl;
+
+  cout << "leaving tests.main" << endl << endl;
   return 0;
 }
